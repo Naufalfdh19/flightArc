8ace723047335937ede436010f1a67229dab6a8b
naufal <naufalfh19@gmail.com>
2026-03-21 23:29:31 +0700

    feat: add new field for schedule entity

--- modules/schedule/dto/schedule.go
+++ modules/schedule/dto/schedule.go
@@ -3,9 +3,10 @@ package dto
 import "time"
 
 type GetScheduleDto struct {
-	Id            int       `json:"id"`
+	Id            string    `json:"id"`
 	Origin        string    `json:"origin"`
 	Destination   string    `json:"destination"`
 	Status        string    `json:"status"`
-	DepartureDate time.Time `json:"departure_date"`
+	DepartureTime time.Time `json:"departure_time"`
+	ArrivalTime   time.Time `json:"arrival_time"`
 }
