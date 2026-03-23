55a485fba983c0c57966dcdb8149f2de6b138e2e
naufal <naufalfh19@gmail.com>
2026-03-23 17:01:59 +0700

    feat: add flight entity

--- modules/schedule/entity/schedule.go
+++ modules/schedule/entity/schedule.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"flight/modules/airport/entity"
 	"time"
 
 	"github.com/shopspring/decimal"
@@ -10,6 +11,16 @@ type Schedule struct {
 	Id            string
 	Origin        string
 	Destination   string
+	Status        string
+	DepartureTime time.Time
+	ArrivalTime   time.Time
+	Price         decimal.Decimal
+}
+
+type Flight struct {
+	Id            string
+	Origin        entity.Airport
+	Destination   entity.Airport
 	Status        string
 	DepartureTime time.Time
 	ArrivalTime   time.Time
