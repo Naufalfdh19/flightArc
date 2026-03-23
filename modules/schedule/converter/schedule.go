8ef5ca60e3035d34dade5fc78a71d52f56c9837b
naufal <naufalfh19@gmail.com>
2026-03-23 17:01:29 +0700

    feat: add get flight converter

--- modules/schedule/converter/schedule.go
+++ modules/schedule/converter/schedule.go
@@ -1,6 +1,7 @@
 package converter
 
 import (
+	airportConverter "flight/modules/airport/converter"
 	"flight/modules/schedule/dto"
 	"flight/modules/schedule/entity"
 )
@@ -12,6 +13,19 @@ func (c GetScheduleConverter) ToDto(schedule entity.Schedule) dto.GetScheduleDto
 		Id:            schedule.Id,
 		Origin:        schedule.Origin,
 		Destination:   schedule.Destination,
+		Status:        schedule.Status,
+		DepartureTime: schedule.DepartureTime,
+		ArrivalTime:   schedule.ArrivalTime,
+	}
+}
+
+type GetFlightConverter struct{}
+
+func (c GetFlightConverter) ToDto(schedule entity.Flight) dto.GetFlightDto {
+	return dto.GetFlightDto{
+		Id:            schedule.Id,
+		Origin:        airportConverter.GetAirportConverter{}.ToDto(schedule.Origin),
+		Destination:   airportConverter.GetAirportConverter{}.ToDto(schedule.Destination),
 		Status:        schedule.Status,
 		DepartureTime: schedule.DepartureTime,
 		ArrivalTime:   schedule.ArrivalTime,
