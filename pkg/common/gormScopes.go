2abf17ca1c4769a97ce1974a40f9065353e4193f
naufal <naufalfh19@gmail.com>
2026-03-25 20:59:26 +0700

    feat: add common scope for query

---
+++ pkg/common/gormScopes.go
@@ -0,0 +1,25 @@
+package common
+
+import "gorm.io/gorm"
+
+// 1. Definisikan Scopes
+func IsNotDeleted(db *gorm.DB) *gorm.DB {
+    return db.Where("deleted_at IS NULL")
+}
+
+func Paginate(page, limit, totalpage int) func(db *gorm.DB) *gorm.DB {
+    return func(db *gorm.DB) *gorm.DB {
+        page = checkPage(page, totalpage)
+        offset := (page - 1) * limit
+        return db.Offset(offset).Limit(limit)
+    }
+}
+
+func checkPage(page, totalpage int) int {
+    if page > totalpage {
+        return totalpage
+    } else if page < 0 {
+        return 1
+    }
+    return page
+}
