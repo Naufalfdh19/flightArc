baa3e034c44dfed3cb5e27878589b60cff785229
naufal <naufalfh19@gmail.com>
2026-03-26 23:17:17 +0700

    feat: add ticket entity

--- modules/booking/entity/booking.go
+++ modules/booking/entity/booking.go
@@ -1,6 +1,11 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
 
 type Booking struct {
 	Id            int
@@ -10,4 +15,14 @@ type Booking struct {
 	SeatNumber    string
 	Status        string
 	BookingTime   time.Time
+}
+
+type Ticket struct {
+	Id            uuid.UUID       `json:"id"`
+	BookingId     uuid.UUID       `json:"booking_id"`
+	IdNumber      string          `json:"id_number"`
+	PassengerName string          `json:"passenger_name"`
+	SeatNumber    string          `json:"seat_number"`
+	Class         string          `json:"class"`
+	Price         decimal.Decimal `json:"price"`
 }
