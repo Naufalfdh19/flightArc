7df4f7a3c5fec515ecd97fd7e598142c65d716c8
naufal <naufalfh19@gmail.com>
2026-03-28 19:12:06 +0700

    fix: ID type for GetBooking DTO

--- modules/booking/dto/booking.go
+++ modules/booking/dto/booking.go
@@ -4,13 +4,15 @@ import (
 	scheduleDto "flight/modules/schedule/dto"
 	userDto "flight/modules/user/dto"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 )
 
-type GetBookingsDto struct {
-	Id            int                      `json:"id"`
-	User          userDto.GetUserResponse  `json:"user"`
-	Flight        scheduleDto.GetFlightDto `json:"flight"`
-	Status        string                   `json:"status"`
-	BookingTime   time.Time                `json:"booking_time"`
+type GetBooking struct {
+	Id          uuid.UUID                      `json:"id"`
+	User        userDto.GetUserResponse  `json:"user"`
+	Flight      scheduleDto.GetFlightDto `json:"flight"`
+	Status      string                   `json:"status"`
+	BookingTime time.Time                `json:"booking_time"`
 }
-
