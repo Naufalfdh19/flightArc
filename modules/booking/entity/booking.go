9aa97016900fb89dd59a2402dcaf5e415523bb23
naufal <naufalfh19@gmail.com>
2026-03-28 19:11:10 +0700

    fix: Booking entity field structure and ticket entity

--- modules/booking/entity/booking.go
+++ modules/booking/entity/booking.go
@@ -1,26 +1,33 @@
 package entity
 
 import (
+	flightEnt "flight/modules/schedule/entity"
+	userEnt "flight/modules/user/entity"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/shopspring/decimal"
+	"gorm.io/gorm"
 )
 
 type Booking struct {
-	Id            int
-	UserId        int
-	FlightId      int
-	Status        string
-	BookingTime   time.Time
+	Id          uuid.UUID
+	UserId      int
+	ScheduleId    uuid.UUID
+	User        userEnt.User    `gorm:"foreignKey:UserId"`
+    Flight      flightEnt.Flight  `gorm:"foreignKey:ScheduleId"`
+	Status      string
+	BookingTime time.Time
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
 }
 
 type Ticket struct {
-	Id            uuid.UUID       `json:"id"`
-	BookingId     uuid.UUID       `json:"booking_id"`
-	IdNumber      string          `json:"id_number"`
-	PassengerName string          `json:"passenger_name"`
-	SeatNumber    string          `json:"seat_number"`
-	Class         string          `json:"class"`
-	Price         decimal.Decimal `json:"price"`
+	Id            uuid.UUID
+	BookingId     uuid.UUID
+	IdNumber      string
+	PassengerName string
+	SeatNumber    string
+	Class         string
+	Price         decimal.Decimal
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
