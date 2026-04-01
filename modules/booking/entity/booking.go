a1605ce3f514497edadda6b19cbaab8df1f06891
naufal <naufalfh19@gmail.com>
2026-04-01 23:23:37 +0700

    delete: ticket entity

--- modules/booking/entity/booking.go
+++ modules/booking/entity/booking.go
@@ -6,28 +6,16 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
 )
 
 type Booking struct {
-	Id          uuid.UUID
+	Id          uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
 	UserId      int
-	ScheduleId    uuid.UUID
-	User        userEnt.User    `gorm:"foreignKey:UserId"`
-    Flight      flightEnt.Flight  `gorm:"foreignKey:ScheduleId"`
+	ScheduleId  uuid.UUID
+	User        userEnt.User     `gorm:"foreignKey:UserId"`
+	Flight      flightEnt.Flight `gorm:"foreignKey:ScheduleId"`
 	Status      string
 	BookingTime time.Time
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
-}
-
-type Ticket struct {
-	Id            uuid.UUID
-	BookingId     uuid.UUID
-	IdNumber      string
-	PassengerName string
-	SeatNumber    string
-	Class         string
-	Price         decimal.Decimal
-	DeletedAt     gorm.DeletedAt `gorm:"index"`
 }
