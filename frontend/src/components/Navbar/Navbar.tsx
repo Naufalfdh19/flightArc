88eb33e19105498a87f391cb7fb8cac08695b73d
naufal <naufalfh19@gmail.com>
2026-04-21 22:06:47 +0700

    feat: add navbar component

---
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import Button from "../Button/Button"
+
+function Navbar() {
+    return (
+        <>  
+            <div className="h-18 w-full flex bg-black">
+                {/* Logo & Brand */}
+                <div className="w-100 flex h-full items-center justify-center border-amber-50">
+                    <h1 className="text-amber-50 text-2xl">FlightArc</h1>
+                </div>
+                {/* Menu */}
+                <div className="h-full w-full px-20">
+                    <div className="h-full w-full flex items-center gap-5 border-amber-50">
+                        <h1 className="text-amber-50">Flights</h1>
+                        <h1 className="text-amber-50">Hotels</h1>
+                    </div>
+                </div>
+                {/* Login & Register */}
+                <div className="h-full w-140 flex items-center justify-center gap-5 border-amber-50 ">
+                    {/* <button className="h-10 w-23 text-amber-50 bg-[#ac8743]  rounded-full">Log In</button>
+                    <button className="h-10 w-23 text-amber-50 bg-[#523a0d]  rounded-full">Register</button> */}
+                    <Button square="full">
+                        Log In
+                    </Button>
+                    <Button type="dark-gold" square="full">
+                        Register
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar
