c2c3c077daf91abecc82f3d1b30556ad00e24fbf
naufal <naufalfh19@gmail.com>
2026-04-22 22:43:04 +0700

    feat: add HomePage

---
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+
+import Navbar from "../../components/Navbar/Navbar"
+
+function HomePage() {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    )
+}
+
+export default HomePage
