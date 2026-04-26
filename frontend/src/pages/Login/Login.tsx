42a9ef58c78094d10b2867e1ef3a2d93708a6c92
naufal <naufalfh19@gmail.com>
2026-04-26 22:05:35 +0700

    feat: create form and connect API to get user JWT

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,22 +1,75 @@
 
+import React, { useState } from "react"
 import Button from "../../components/Button/Button"
-
-
+import { useNavigate } from "react-router-dom"
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    const [email, setEmail ] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function loginSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        console.log({email, password})
+        const res = await fetch("http://localhost:9000/api/v1/user/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        })
+
+        const resData = await res.json()
+        
+        console.log(resData)
+    }
+
     return (
         <>
             <div className="h-screen w-screen flex">
                 <div className="h-full w-1/2 bg-amber-500"></div>
                 <div className="h-full w-1/2 bg-black flex items-center justify-center">
-                    <div className="h-[50%] w-[75%]">
+                    <div className="h-[75%] w-[75%]">
                         <p className="text-[#ac8743]">Welcome Back</p>
-                        <p className="my-2 text-amber-50 text-6xl">Sign in to your account</p>
-                        <span className="pt-1 text-amber-50">New to ArcFlight?</span> <span className="text-[#ac8743]">Create a free account</span>
+                        <p className="mt-2 mb-5 text-amber-50 text-6xl">Sign in to your account</p>
+                        <span className="text-amber-50">New to ArcFlight? </span> 
+                        <span 
+                            onClick={() => navigate("/register")} 
+                            className="text-amber-500 hover:text-amber-300 cursor-pointer">Create a free account
+                        </span>
                         <div className="my-10 gap-5 flex">
                             <Button border="white-1" width="md" height="md" type="black" square="full">Google</Button>
                             <Button border="white-1" width="md" height="md" type="black" square="full">Apple</Button>
                         </div>
+                        <form onSubmit={loginSubmit} className="flex flex-col gap-5">
+                            <input 
+                                placeholder="Email" 
+                                type="text" 
+                                className="h-10 p-2 bg-gray-300 rounded-sm" 
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+
+                            <input 
+                                placeholder="Password" 
+                                type="password" 
+                                className="h-10 p-2 bg-gray-300 rounded-sm" 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                            <Button 
+                                height="sm" type="primary-700" square="sm"
+                                isSubmit={true}
+                                width="w-[100%]"
+                            > Log In
+                            </Button>
+                        </form>
                     </div>
                     
                 </div>
