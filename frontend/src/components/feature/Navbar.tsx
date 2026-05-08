e66a2b24f7a97999474383f43128d2914be2857f
naufal <naufalfh19@gmail.com>
2026-05-08 20:09:08 +0700

    fix new file location

---
+++ frontend/src/components/feature/Navbar.tsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom"
+import Button from "../ui/Button"
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    return (
+        <>  
+            <div className="h-18 w-full flex bg-white/1">
+                {/* Logo & Brand */}
+                <div className="w-100 flex h-full items-center justify-center border-amber-50">
+                    <h1 className="text-amber-50 text-2xl">FlightArc</h1>
+                </div>
+                {/* Menu */}
+                <div className="h-full w-full px-20">
+                    <div className="h-full w-full flex items-center gap-5 border-amber-50">
+                        <h1 className="text-amber-50">Flights</h1>
+                        <h1 className="text-amber-50">Hotels</h1>
+                    </div>
+                </div>
+                {/* Login & Register */}
+                <div className="h-full w-140 flex items-center justify-center gap-5 border-amber-50 ">
+                    {/* <button className="h-10 w-23 text-amber-50 bg-[#ac8743]  rounded-full">Log In</button>
+                    <button className="h-10 w-23 text-amber-50 bg-[#523a0d]  rounded-full">Register</button> */}
+                    <Button onClick={() => navigate("/login")} square="full">
+                        Log In
+                    </Button>
+                    <Button onClick={() => navigate("/register")} type="primary-700" square="full">
+                        Register
+                    </Button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar
