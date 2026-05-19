dac7a871f8b0f3b03298bf648e86707d4c3e61d2
naufal <naufalfh19@gmail.com>
2026-05-19 22:16:28 +0700

    feat: add filter for get flights

--- backend/modules/schedule/queryparams/schedule.go
+++ backend/modules/schedule/queryparams/schedule.go
@@ -5,11 +5,19 @@ import (
 )
 
 type QueryParams struct {
+	OriginCode string
+	OriginCity string
+	DestinationCode string
+	DestinationCity string
 	Page  int
 	Limit int
 }
 
 type QueryParamsDto struct {
+	OriginCode string `form:"origin_code"`
+	OriginCity string `form:"origin_city"`
+	DestinationCode string `form:"destination_code"`
+	DestinationCity string `form:"destination_city"`
 	Page  int `form:"page"`
 	Limit int `form:"limit"`
 }
@@ -18,9 +26,32 @@ type QueryParamsConverter struct{}
 
 func (c QueryParamsConverter) ConvertDtoToEntity(queryparamsDto QueryParamsDto) QueryParams {
 	return QueryParams{
+		OriginCode: queryparamsDto.OriginCode,
+		OriginCity: queryparamsDto.OriginCity,
+		DestinationCode: queryparamsDto.DestinationCode,
+		DestinationCity: queryparamsDto.DestinationCity,
 		Page:  queryparamsDto.Page,
 		Limit: queryparamsDto.Limit,
 	}
+}
+
+func AddFilter(queryParams QueryParams) string {
+	filter := ""
+
+	if (queryParams.OriginCity != "") {
+		filter += fmt.Sprintf(" AND s.origin_city = '%s'", queryParams.OriginCity)
+	}
+	if (queryParams.OriginCode != "") {
+		filter += fmt.Sprintf(" AND s.origin_code = '%s'", queryParams.OriginCode)
+	}
+	if (queryParams.DestinationCity != "") {
+		filter += fmt.Sprintf(" AND s.destination_city = '%s'", queryParams.DestinationCity)
+	}
+	if (queryParams.DestinationCode != "") {
+		filter += fmt.Sprintf(" AND s.destination_code = '%s'", queryParams.DestinationCode)
+	}
+
+	return filter
 }
 
 func AddPagination(queryParams QueryParams) string {
